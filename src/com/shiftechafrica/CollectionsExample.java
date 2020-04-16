da888ce2dc1bfda1dc7917deddc7fe71b6fcecec
techguy <jobvinny@1993>
2020-04-16 11:35:33 +0300

    :art: Implementations of the collection interface.

---
+++ src/com/shiftechafrica/CollectionsExample.java
@@ -0,0 +1,12 @@
+package com.shiftechafrica;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsExample {
+    CollectionsExample() {
+        List<Integer> list1 = Arrays.asList(1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
+        System.out.println("Position of 21 is " + Collections.binarySearch(list1, 21));
+    }
+}
